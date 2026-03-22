032622e5f6a41a16e59cb156764e43ab973893ec
Javier Gatón Herguedas <gaton@goa.uva.es>
2026-03-22 20:51:06 +0100

    added database common interface , insertion chunk_size customization, and insertion callback for each chunk

--- goadb/sql/db.py
+++ goadb/sql/db.py
@@ -8,6 +8,7 @@ import sqlalchemy.exc
 import pandas as pd
 
 from goadb.sql.models import DBConfig
+from goadb.common.interface import IDataBase
 
 
 _MYSQL_DRIVERS= [
@@ -52,7 +53,7 @@ def _get_sql_engine(cf: DBConfig) -> sqlalchemy.Engine:
     return sqlalchemy.create_engine(url)
 
 
-class DataBase:
+class DataBase(IDataBase):
     """Representation of a mysql database using sqlalchemy logic down below.
 
     Attributes
@@ -128,10 +129,12 @@ class DataBase:
         df: pd.DataFrame,
         table: str,
         ignore: bool = True,
+        callback_iter: Callable = None,
+        chunk_size: int = 4000,
     ) -> int:
         """Inserts the given DataFrame into the DB in the table `table`.
 
-        It will do it in multiple commits of size 10000.
+        It will do it in multiple commits of size `chunk_size`.
 
         Parameters
         ----------
@@ -142,6 +145,12 @@ class DataBase:
             Name of the table that will contain the dataframe rows.
         ignore: bool
             Behaviour in case of colition. Ignore or update. By default ignore.
+        callback_iter: Callable
+            If defined, callback that will be called with the accumulated number of rows sent
+            to the database. The callback will be called each time a new chunk insertion is
+            performed.
+        chunk_size: int
+            Chunk insertion size. By default it's 4000.
 
         Returns
         -------
@@ -171,8 +180,10 @@ class DataBase:
             return 0
         engine = self._get_engine()
         inserts = 0
-        step = 200
+        step = int(chunk_size)
         for i in range(0, len(df), step):
+            if callback_iter:
+                callback_iter(i)
             inserts += df.iloc[i : i + step].to_sql(
                 table,
                 engine,
